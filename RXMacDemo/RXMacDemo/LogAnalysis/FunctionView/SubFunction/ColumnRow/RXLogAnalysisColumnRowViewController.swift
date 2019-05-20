1df9f889a554c7e2b7b466cf4d2ca25dd8ba694c
xuzhijun <xuzhijun@vipkid.com.cn>
2019-05-20 22:00:23 +0800

    add

---
+++ RXMacDemo/RXMacDemo/LogAnalysis/FunctionView/SubFunction/ColumnRow/RXLogAnalysisColumnRowViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RXLogAnalysisColumnRowViewController.swift
+//  RXMacDemo
+//
+//  Created by Rush.D.Xzj on 2019/5/20.
+//  Copyright © 2019 Rush.D.Xzj. All rights reserved.
+//
+
+import Cocoa
+
+class RXLogAnalysisColumnRowViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
